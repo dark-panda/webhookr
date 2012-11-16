d84423f6c3845943519272531773ec30265105d9
Gerry Power <gerry@zoocasa.com>
2012-11-15 21:15:53 -0500

    Add Mandrill protocol & tests

---
+++ app/models/webhookr/invalid_protocol_error.rb
@@ -0,0 +1,3 @@
+module Webhookr
+  class InvalidProtocolError < RuntimeError; end
+end
---
+++ app/models/webhookr/mandrill/protocol.rb
@@ -0,0 +1,35 @@
+module Webhookr
+  class Mandrill::Protocol
+    EVENT_KEY = 'event'
+    EVENT_DATE_KEY = 'ts'
+    DATA_KEY = "msg"
+
+    class << self
+      def process(raw_response)
+        raise(Webhookr::InvalidProtocolError, "Unknown response |#{raw_response.inspect}|") unless raw_response =~ /mandrill_events=/
+
+        p = ActiveSupport::JSON.decode(CGI.unescape(raw_response).gsub(/mandrill_events=/,""))
+        assert_valid_packet(p)
+
+        p.collect do |item|
+          OpenStruct.new(:event_type => item[EVENT_KEY],
+                         :fired_at => item[EVENT_DATE_KEY],
+                         :protocol => name,
+                         :data => OpenStruct.new(item[DATA_KEY]))
+        end
+      end
+
+      def assert_valid_packet(p)
+        raise(Webhookr::InvalidProtocolError, "Malformed response |#{p.inspect}|") unless p.is_a?(Array) && p.first.is_a?(Hash)
+
+        p.each do |item|
+          raise(Webhookr::InvalidProtocolError, "Unknown event #{item[EVENT_KEY]}") unless item[EVENT_KEY]
+          raise(Webhookr::InvalidProtocolError, "No msg in the response") unless item[DATA_KEY]
+        end
+      end
+    end
+
+    private_class_method :assert_valid_packet
+
+  end
+end
