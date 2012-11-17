07f18a4842f80dbf91a8db305b7574bdc4045ae4
Gerry Power <gerry@zoocasa.com>
2012-11-17 18:03:15 -0500

    Convert to instance methods

--- app/models/webhookr/response_handler.rb
+++ app/models/webhookr/response_handler.rb
@@ -1,25 +1,27 @@
 module Webhookr
   class ResponseHandler
     
-    class << self
-      def process(service_name, body)
-        service_protocol_class(service_name).send(:process, body).each do |r|
-          klass = service_call_back_class(service_name)
-          method = r.event_type
-          klass.send(method, r.data) if klass.respond_to?(method)
-        end
-      end
+    def self.process(service_name, body)
+      new.process(service_name, body)
+    end
 
-      def service_protocol_class(service_name)
-        ("Webhookr::" + service_name.camelize + "::Protocol").constantize
-      end
-
-      def service_call_back_class(service_name)
-        Object.const_get((service_name + "_webhook").camelize)
+    def process(service_name, body)
+      service_protocol_class(service_name).send(:process, body).each do |r|
+        klass = service_call_back_class(service_name)
+        method = r.event_type
+        klass.send(method, r.data) if klass.respond_to?(method)
       end
     end
 
-    private_class_method :service_protocol_class, :service_call_back_class
+    private
+
+    def service_protocol_class(service_name)
+      ("Webhookr::" + service_name.camelize + "::Protocol").constantize
+    end
+
+    def service_call_back_class(service_name)
+      Object.const_get((service_name + "_webhook").camelize)
+    end
 
   end
 end
