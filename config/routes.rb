757c955ae25872467686bfdbdf70abb52a53bc28
Gerry Power <gerry@thepowerhouse.com>
2013-07-18 23:22:14 -0400

    Default and constrain requests to json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Webhookr::Engine.routes.draw do
-  get "/events/:service_id(/:security_token)" => "events#show", :as => "events"
-  post "/events/:service_id(/:security_token)" => "events#create", :as => "new_event"
+  get "/events/:service_id(/:security_token)" => "events#show",
+      :as => "events",
+      :defaults => { :format => 'json' },
+      :constraints => { :format => 'json' }
+  post "/events/:service_id(/:security_token)" => "events#create",
+       :as => "new_event",
+       :defaults => { :format => 'json' },
+       :constraints => { :format => 'json' }
 end
